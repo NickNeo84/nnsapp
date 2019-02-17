20af71b9d8c761b5977d675767bfc0aae00cb212
Nick <nikolay.neoberdin@yandex.ru>
2019-02-17 22:10:57 +0300

    make it better

---
+++ myapp/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/myapp'));
+
+app.get('./*', (req,res) =>{
+    res.sendfile(path.join(__dirname,'/dist/myapp/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('start');   
+})
